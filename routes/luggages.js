5d8ea47421fb045057c8dca61251818b87d668bd
naushadbegum <nau.shad@hotmail.com>
2022-12-25 12:55:08 +0000

    adding the form to create page in progress

--- routes/luggages.js
+++ routes/luggages.js
@@ -2,11 +2,38 @@ const express = require("express");
 const router = express.Router();
 
 const {Luggage} = require('../models')
+const {bootstrapField, createLuggageForm} = require('../forms');
 
 router.get('/', async (req,res)=> {
     let luggages = await Luggage.collection().fetch();
     res.render('luggages/index', {
         'luggages': luggages.toJSON()
+    })
+});
+
+router.get('/create', async (req,res)=>{
+    const luggageForm = createLuggageForm();
+    res.render('luggages/create',{
+        'form': luggageForm.toHTML(bootstrapField)
+    })
+});
+
+router.post('/create', async(req,res)=>{
+    const luggageForm = createLuggageForm();
+    luggageForm.handle(req, {
+        'success': async (form) => {
+            const luggage = new Luggage();
+            luggage.set('model', form.data.model);
+            luggage.set('cost', form.data.cost);
+            luggage.set('description', form.data.description);
+            await luggage.save();
+            res.redirect('/luggages');
+        },
+        'error': async (form) => {
+            res.render('luggage/create', {
+                'form': form.toHTML(bootstrapField)
+            })
+        }
     })
 })
 

0b23bc8363cc2faf7fc7fe509f25e0836e7e3612
naushadbegum <nau.shad@hotmail.com>
2023-01-04 11:52:56 +0000

    api luggages debug res.send to object

--- routes/api/luggages.js
+++ routes/api/luggages.js
@@ -3,14 +3,15 @@ const router = express.Router();
 
 const luggageDataLayer = require('../../dal/luggages')
 
-router.get('/', async (req, res)=> {
-    res.send(await luggageDataLayer.getAllLuggages())
-})
+// router.get('/', async (req, res)=> {
+//     res.send(await luggageDataLayer.getAllLuggages())
+// })
 
-router.get('/search', async(req,res)=>{
+router.get('/', async(req,res)=>{
     const searchFields = req.query;
     const luggages = await luggageDataLayer.searchLuggages(searchFields);
-    res.send(luggages)
+    res.send({luggages})
+    // console.log({luggages});
     }
 )
 
@@ -30,16 +31,15 @@ router.get('/search_options', async function (req, res){
         materials,
         types
     }
-
-    res.send(options)
+    res.send({options})
 
 })
 
 router.get(':/luggage_id', async function (req, res){
 
     const luggage = await luggageDataLayer.getLuggageById(req.params.luggage_id);
-
-    res.send(luggage)
+    
+    res.send({luggage})
 })
 
 module.exports = router;
